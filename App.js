6110bf01fb82ce27688faeedfe2f2a195f5796de
JEGAJITH <114394648+jega2404@users.noreply.github.com>
2022-11-25 16:42:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import RegisterForm from './form';
+function App() {
+  return (
+    <div className="App">
+      <RegisterForm/>
+    </div>
+  );
+}
+
+export default App;
